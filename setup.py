451fab4b1e5dc9614d09d246c573949451f6644c
Mayur Harisangam <mayurharisangam10@gmail.com>
2025-03-28 14:57:09 +0530

    Added setup.py and requirement.txt

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Finance-Chatbot",
+    version= '0.0.1',
+    author="Mayur Harisangam",
+    author_email="mayurharisangam10@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
